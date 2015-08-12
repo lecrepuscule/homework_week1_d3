fcc7fd9f96210dde754b7faf628bbf2e560555d8
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-12 17:36:13 +0100

    added a basic input constraint for numbers

--- calc_it/calc.js
+++ calc_it/calc.js
@@ -1,7 +1,17 @@
-var op = prompt('choose an operation');
-console.log('op', op)
 
-var num = prompt('choose a number');
-console.log('num', num)
+alert("This is a calculator, it will ask you for a number, an operator, and another number, in that order, then perform the arithmetic operation on those two numbers. Click 'OK' to start.");
 
-alert('Why is your console not open?');
+var firstNum = prompt('Choose the first number.');
+
+while ( isNaN(Number(firstNum)) ) {
+  firstNum = prompt('Please enter a number.');
+}
+
+
+// var op = prompt('choose an operation');
+// console.log('op', op)
+
+// var num = prompt('choose a number');
+// console.log('num', num)
+
+// alert('Why is your console not open?');
