d4fa83ee84d4dbcd338e598c1323527bf80ba8b9
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-12 18:08:01 +0100

    added reusable steps into a function

--- calc_it/calc.js
+++ calc_it/calc.js
@@ -1,15 +1,20 @@
 
 alert("This is a calculator, it will ask you for a number, an operator, and another number, in that order, then perform the arithmetic operation on those two numbers. Click 'OK' to start.");
 
-var firstUserInput = prompt("Choose the first number.");
 var firstNum;
+var secondNum;
+var operator;
 
-while ( isNaN(Number(firstUserInput)) ) {
-  firstUserInput = prompt("Please enter a number.");
+
+function getValidNumber () {
+  var userInput = prompt("Choose the first number.");
+  while ( isNaN(Number(userInput)) ) {
+    userInput = prompt("Please enter a number.");
+  }
+  return Number(userInput);
 }
 
-firstNum = Number(firstUserInput);
-
+firstNum = getValidNumber();
 
 // var operator = prompt("Please choose '+' for addition, '-' for subtraction")
 
