6ebb6adafbad8d001da67ef3de470a5271abca2f
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-12 17:52:08 +0100

    slightly restructured the code

--- calc_it/calc.js
+++ calc_it/calc.js
@@ -1,12 +1,19 @@
 
 alert("This is a calculator, it will ask you for a number, an operator, and another number, in that order, then perform the arithmetic operation on those two numbers. Click 'OK' to start.");
 
-var firstNum = prompt('Choose the first number.');
+var firstUserInput = prompt("Choose the first number.");
+var firstNum;
 
-while ( isNaN(Number(firstNum)) ) {
-  firstNum = prompt('Please enter a number.');
+while ( isNaN(Number(firstUserInput)) ) {
+  firstUserInput = prompt("Please enter a number.");
 }
 
+firstNum = Number(firstUserInput);
+
+
+// var operator = prompt("Please choose '+' for addition, '-' for subtraction")
+
+// var validOperator = 
 
 // var op = prompt('choose an operation');
 // console.log('op', op)
